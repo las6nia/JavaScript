614e8f2c910e6c4322bdb9fd4fe18f8d28ecd5bc
las6nia <lalcalaroblero2@gmail.com>
2025-07-29 13:16:11 -0500

    Add files via upload

---
+++ newperson.js
@@ -0,0 +1,44 @@
+/*const collegeSt= {
+  firstName: "Luis",
+  occupation: "College Student.",
+  address: "108 Brown Quail",
+  gpa: 3.2,
+  major: "Cybersecurity",
+  education: "UWGB",
+  age: 21,
+  eyeColor: "brown"
+};
+console.log(collegeSt.firstName+" is a "+ collegeSt.occupation+" He lives at "+ collegeSt.address +", his GPA is "+collegeSt.gpa+", his major is "+collegeSt.major+"and he goes to "+collegeSt.education +". He is "+collegeSt.age);
+
+
+function highschoolSt(base){
+  this.firstName = "Alex";
+  this.occupation = "High school student.";
+  this.address = "1713 onsgard rd"
+  this.gpa = base.gpa;
+  this.education = "UW-Madison"
+  this.age = base.age;
+  this.annualsalary = "21,000/year";        //double check with teacher
+  
+};
+
+const newhighschoolSt = new highschoolSt(collegeSt);
+
+console.log(newhighschoolSt.firstName +" has a GPA of "+ nexwhighschoolSt.gpa +"."+" He lives at "+newhighschoolSt.address + " and he goes to "+newhighschoolSt.education)
+
+
+function career(base){
+  let basepay = ${200000}; //recall information on this - https://stackoverflow.com/questions/35835362/what-does-dollar-sign-and-curly-braces-mean-in-a-string-in-javascript
+
+
+}
+  */
+
+var foo = 'bar';
+console.log(`Let's meet at the ${foo}`);
+// Let's meet at the bar
+
+
+let food = "sushi";
+console.log('let's east some {food}');  
+

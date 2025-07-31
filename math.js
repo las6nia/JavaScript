c36866532aade50ce49232de846d7d9b4705e542
las6nia <lalcalaroblero2@gmail.com>
2025-07-30 19:56:34 -0500

    Create math.js

---
+++ math.js
@@ -0,0 +1,59 @@
+
+let john = 7;
+let friend = 5;
+
+total = friend +john;
+
+console.log("john has "+total+ " apples total")
+
+//comments -    -   -   -   //
+
+let seconds= 60;
+let minutes=3;
+
+video= minutes*seconds;
+console.log("you watched " +video + " seconds in "+(minutes)+" minutes")
+
+//comments -    -   -   -   //
+
+let speed = 310;
+let hours = 2;
+
+total$speed = speed/hours;
+
+console.log("The speed would be "+ (total$speed)+" miles per hour")
+
+//comments -    -   -   -   //
+
+let pens = 2;
+let notebook= 5;
+
+penstotal = pens*3;
+notebooktotal = notebook*2;
+
+var totalcost= penstotal+notebooktotal;
+console.log("The total cost would be $"+(totalcost));
+
+
+
+cupcakes = 20;
+friends = 4;
+
+cupperfriend= cupcakes/friends;
+console.log("Each friend would get "+(cupperfriend)+" cupcakes each");
+
+
+
+let candy = 35;
+let kids = 6;
+
+//comments -    -   -   -   //
+total = Math.floor(candy / kids);
+
+let leftover = candy % kids; - //Study!!!!!!!!!!
+
+console.log("Each kid gets " + (total) + " and " + (leftover) + " are left over.");
+
+
+
+console.log()
